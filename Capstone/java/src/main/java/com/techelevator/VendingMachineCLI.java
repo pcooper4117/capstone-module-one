6ce5734bf0e8b3057dc15e074231a1dd5d25577a
Frank Fella <frank@techelevator.com>
2021-02-02 19:18:07 -0500

    Instructor Push 02/02/2021 19:18:07

---
+++ Capstone/java/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -0,0 +1,86 @@
+package com.techelevator;
+/**************************************************************************************************************************
+*  This is your Vending Machine Command Line Interface (CLI) class
+*
+*  It is the main process for the Vending Machine
+*
+*  THIS is where most, if not all, of your Vending Machine interactions should be coded
+*  
+*  It is instantiated and invoked from the VendingMachineApp (main() application)
+*  
+*  Your code vending machine related code should be placed in here
+***************************************************************************************************************************/
+import com.techelevator.view.Menu;         // Gain access to Menu class provided for the Capstone
+
+public class VendingMachineCLI {
+
+    // Main menu options defined as constants
+
+	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
+	private static final String MAIN_MENU_OPTION_PURCHASE      = "Purchase";
+	private static final String MAIN_MENU_OPTION_EXIT          = "Exit";
+	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS,
+													    MAIN_MENU_OPTION_PURCHASE,
+													    MAIN_MENU_OPTION_EXIT
+													    };
+	
+	private Menu vendingMenu;              // Menu object to be used by an instance of this class
+	
+	public VendingMachineCLI(Menu menu) {  // Constructor - user will pas a menu for this class to use
+		this.vendingMenu = menu;           // Make the Menu the user object passed, our Menu
+	}
+	/**************************************************************************************************************************
+	*  VendingMachineCLI main processing loop
+	*  
+	*  Display the main menu and process option chosen
+	*
+	*  It is invoked from the VendingMachineApp program
+	*
+	*  THIS is where most, if not all, of your Vending Machine objects and interactions 
+	*  should be coded
+	*
+	*  Methods should be defined following run() method and invoked from it
+	*
+	***************************************************************************************************************************/
+
+	public void run() {
+
+		boolean shouldProcess = true;         // Loop control variable
+		
+		while(shouldProcess) {                // Loop until user indicates they want to exit
+			
+			String choice = (String)vendingMenu.getChoiceFromOptions(MAIN_MENU_OPTIONS);  // Display menu and get choice
+			
+			switch(choice) {                  // Process based on user menu choice
+			
+				case MAIN_MENU_OPTION_DISPLAY_ITEMS:
+					displayItems();           // invoke method to display items in Vending Machine
+					break;                    // Exit switch statement
+			
+				case MAIN_MENU_OPTION_PURCHASE:
+					purchaseItems();          // invoke method to purchase items from Vending Machine
+					break;                    // Exit switch statement
+			
+				case MAIN_MENU_OPTION_EXIT:
+					endMethodProcessing();    // Invoke method to perform end of method processing
+					shouldProcess = false;    // Set variable to end loop
+					break;                    // Exit switch statement
+			}	
+		}
+		return;                               // End method and return to caller
+	}
+/********************************************************************************************************
+ * Methods used to perform processing
+ ********************************************************************************************************/
+	public void displayItems() {      // static attribute used as method is not associated with specific object instance
+		// Code to display items in Vending Machine
+	}
+	
+	public void purchaseItems() {	 // static attribute used as method is not associated with specific object instance
+		// Code to purchase items from Vending Machine
+	}
+	
+	public void endMethodProcessing() { // static attribute used as method is not associated with specific object instance
+		// Any processing that needs to be done before method ends
+	}
+}
